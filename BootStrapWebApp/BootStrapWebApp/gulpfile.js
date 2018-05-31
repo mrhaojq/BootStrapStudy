bb08dc54f66d612a10c6c49048a365267f9ca17b
mrhao <mrhao@jiawel>
2018-05-31 19:36:01 +0800

    bootstrap

---
+++ BootStrapWebApp/BootStrapWebApp/gulpfile.js
@@ -0,0 +1,17 @@
+﻿var gulp = require('gulp');
+
+gulp.task('default', function () {
+
+    gulp.src("bower_components/bootstrap/dist/**", { base: 'bower_components' })
+        .pipe(gulp.dest('wwwroot/lib'));
+
+    gulp.src("bower_components/jquery/dist/**", { base: 'bower_components' })
+        .pipe(gulp.dest('wwwroot/lib'));
+
+    gulp.src("bower_components/jquery-validation/dist/**", { base: 'bower_components' })
+        .pipe(gulp.dest('wwwroot/lib'));
+
+    gulp.src("bower_components/jquery-validation-unobtrusive/dist/**", { base: 'bower_components' })
+        .pipe(gulp.dest('wwwroot/lib'));
+
+});
